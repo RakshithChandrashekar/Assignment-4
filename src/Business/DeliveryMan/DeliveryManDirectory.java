96ec14486601ea5e768cc59baec8d5304f0b8b9b
RakshithChandrashekar <chandrashekar.r@northeastern.edu>
2021-03-31 02:37:41 -0400

    partial code

---
+++ src/Business/DeliveryMan/DeliveryManDirectory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.DeliveryMan;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author harold
+ */
+public class DeliveryManDirectory {
+    
+    ArrayList<DeliveryMan> deliveryManList = new ArrayList<DeliveryMan>();
+
+    public ArrayList<DeliveryMan> getDeliveryManList() {
+        return deliveryManList;
+    }
+
+    public void setDeliveryManList(ArrayList<DeliveryMan> deliveryManList) {
+        this.deliveryManList = deliveryManList;
+    }
+    
+    public void addDeliveryMan(DeliveryMan deliveryMan){
+        System.out.println(deliveryMan.getFirstname());
+        deliveryManList.add(deliveryMan);
+    }
+    public void removeDeliveryMan(DeliveryMan deliveryMan){
+        deliveryManList.remove(deliveryMan);
+    }
+    
+}
